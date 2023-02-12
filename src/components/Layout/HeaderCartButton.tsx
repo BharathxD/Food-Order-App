de327f8c23a1070a8f6adee5396c05f65c558815
Bharath Lakshman Kumar <2110520062@klh.edu.in>
2023-02-12 22:40:24 +0530

    Added useEffect for header Button Animation

--- src/components/Layout/HeaderCartButton.tsx
+++ src/components/Layout/HeaderCartButton.tsx
@@ -1,4 +1,4 @@
-import React, { useContext } from "react";
+import React, { useContext, useState, useEffect } from "react";
 import CartContext from "../../Store/cart-context";
 import { CartIcon } from "../Cart/CartIcon";
 import classes from "./HeaderCartButton.module.css";
@@ -6,16 +6,34 @@ import classes from "./HeaderCartButton.module.css";
 export const HeaderCartButton: React.FC<{ onClickHandler: () => void }> = (
   props
 ) => {
+  const [buttonIsHighlighted, setButtonIsHighlighted] =
+    useState<boolean>(false);
   const context = useContext(CartContext);
-  const noOfCartItems = context.items.reduce(
+  const { items } = context;
+  const noOfCartItems = items.reduce(
     (currentNumber, item: { amount: number }) => {
       return currentNumber + item.amount;
     },
     0
   );
 
+  const btnClasses = `${classes.button} ${
+    buttonIsHighlighted ? classes.bump : ""
+  }`;
+
+  useEffect(() => {
+    if (items.length === 0) return;
+    setButtonIsHighlighted(true);
+    const cleaner = setTimeout(() => {
+      setButtonIsHighlighted(false);
+    }, 300);
+    return () => {
+      clearTimeout(cleaner);
+    };
+  }, [items]);
+
   return (
-    <button className={classes.button} onClick={props.onClickHandler}>
+    <button className={btnClasses} onClick={props.onClickHandler}>
       <span className={classes.icon}>
         <CartIcon />
       </span>
