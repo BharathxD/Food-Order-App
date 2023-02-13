7fcc9700291e34d08b69d54e01650ffd4611d6de
Bharath Lakshman Kumar <2110520062@klh.edu.in>
2023-02-13 20:06:44 +0530

    Used useMemo to optimize the code bit further

--- src/components/Meals/AvailableMeals.tsx
+++ src/components/Meals/AvailableMeals.tsx
@@ -1,20 +1,25 @@
+import { useMemo } from "react";
 import classes from "./AvailableMeals.module.css";
 import { Card } from "../UI/Card";
 import { MealItem } from "./MealItem/MealItem";
 import { DUMMY_MEALS } from "../../Store/DUMMY_MEALS";
 
 export const AvailableMeals = () => {
-  const mealsList = DUMMY_MEALS.map((meal) => {
-    return (
-      <MealItem
-        key={meal.id}
-        id={meal.id}
-        name={meal.name}
-        description={meal.description}
-        price={meal.price}
-      />
-    );
-  });
+  const mealsList = useMemo(
+    () =>
+      DUMMY_MEALS.map((meal) => {
+        return (
+          <MealItem
+            key={meal.id}
+            id={meal.id}
+            name={meal.name}
+            description={meal.description}
+            price={meal.price}
+          />
+        );
+      }),
+    []
+  );
   return (
     <section className={classes.meals}>
       <Card>
